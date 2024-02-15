f4f10b12da05513199d9848faa2a77093ca059af
JJThomtin <129408043+JJThomtin@users.noreply.github.com>
2024-02-15 21:07:46 +0200

    Implemented player choice to pick first role (#2)

--- game.rb
+++ game.rb
@@ -4,10 +4,29 @@ class Game
     puts "Game initialize"
   end
 
-  def gameloop
+  def start
+    puts "Welcome to the mastermind game"
+    gameloop
   end
+
+  def gameloop
+    starting_roles = pick_player_role
+  end
+
   #pick if player wants to play as the codebreaker or codemaker
   def pick_player_role
+    puts "Which side do you want to start at"
+    puts "'Codebreaker' or 'Codemaker'"
+    loop do
+    player_choice = gets.chomp
+    if player_choice.downcase == 'codemaker'
+      return "codemaker"
+    elsif player_choice.downcase == 'codebreaker'
+      return "codebreaker"
+    else
+      puts "Invalid Input"
+    end
+    end
   end
 
   #Ask player if they want to play again
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 require_relative 'game'
 
 mastermind = Game.new
+mastermind.start
