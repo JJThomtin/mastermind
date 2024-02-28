376bc1412a372450d76ffb03ee55833fca1a3d85
JJThomtin <129408043+JJThomtin@users.noreply.github.com>
2024-02-28 12:26:30 +0200

    Restarted Project (#11)

--- test.rb
+++
@@ -1,4 +0,0 @@
-possible_colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-answer = ["red", "unknown", "green", "unknown"]
-random_guesses = (0..possible_colors.length-1).to_a.sample(answer.tally["unknown"])
-puts random_guesses.inspect
